9656a21d4efd58f3572e52a2dbbc96839da78bd6
Karolina Michalak <karolina0michalak@gmail.com>
2019-12-30 17:37:18 +0100

    created functions for AddTodo form

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,15 +1,41 @@
 import React from 'react';
 
-const AddTodo = () => {
-    return(
-        <div>
+class AddTodo extends React.Component {
+    minDate = new Date().toISOString().slice(0, 10);
+
+    state = {
+        text: '',
+        checked: false,
+        date: this.minDate
+    }
+
+    handleInputChange = e => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    handleCheckboxChange = e => {
+        this.setState({
+            checked: e.target.checked
+        })
+    }
+
+
+
+    render(){
+        let maxDate = this.minDate.slice(0, 4) * 1 + 1;
+        maxDate = maxDate + '-12-31'
+        return(
             <div>
-                <input type="text" placeholder="to to" />
-                <label><input type="checkbox"/> important</label>
+                <div>
+                    <input type="text" placeholder="to do" value={this.state.text} onChange={this.handleInputChange}/>
+                    <label><input type="checkbox" checked={this.state.checked} onChange={this.handleCheckboxChange}/> important</label>
+                </div>
+                    <label>deadline date: <input type="date" min={this.minDate} max={maxDate}/></label>
+                    <button onClick={this.addTask}>add</button>
             </div>
-                <input type="date"/>
-                <button>add</button>
-        </div>
-    )
+        )
+    }
 }
 export default AddTodo
