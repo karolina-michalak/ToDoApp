2d2c5ec2d14bcc3c0dcba289b49d452340a9c26a
Karolina Michalak <karolina0michalak@gmail.com>
2019-12-30 17:10:41 +0100

    added buttons to Todo items

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Todo = props => {
 
-
-
     return(
         <div>
-            {props.task.text}
+            {props.task.text} <button>done</button><button>x</button>
         </div>
     )
 }
