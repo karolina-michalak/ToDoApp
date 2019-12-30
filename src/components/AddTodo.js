c605e201a042c5e91c9cdb1ef5fd27c41b8be328
Karolina Michalak <karolina0michalak@gmail.com>
2019-12-30 17:29:59 +0100

    created AddTodo form

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const AddTodo = () => {
     return(
-        null
+        <div>
+            <div>
+                <input type="text" placeholder="to to" />
+                <label><input type="checkbox"/> important</label>
+            </div>
+                <input type="date"/>
+                <button>add</button>
+        </div>
     )
 }
 export default AddTodo
