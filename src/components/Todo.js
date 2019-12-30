f897044af0889be77c1af1f9ded101a8f399f49d
Karolina Michalak <karolina0michalak@gmail.com>
2019-12-30 17:19:35 +0100

    changing color to red when important

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 const Todo = props => {
 
+    const importantColor = {
+        color: 'red'
+    }
+
+    const {text, date, id, active, important, finishDate} = props.task;
+
+    if(active){
     return(
         <div>
-            {props.task.text} <button>done</button><button>x</button>
+            <div style={important ? importantColor : null}>{props.task.text}</div><button>done</button><button>x</button>
         </div>
-    )
+    )}
+    else{
+        return(
+            <div>
+            {props.task.text} <button>x</button>
+        </div>
+        )
+    }
 }
 export default Todo
